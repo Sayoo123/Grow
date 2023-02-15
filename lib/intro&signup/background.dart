9045fbad09a0f23faa07a4ee49260412966f3e1e
Sayoo123 <sayoojsunil2000@gmail.com>
2023-02-16 00:24:36 +0530

    button updated

--- lib/intro&signup/background.dart
+++ lib/intro&signup/background.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
+// import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'elevatedbutton.dart';
 
 class Background extends StatelessWidget {
   const Background({super.key});
@@ -10,11 +11,11 @@ class Background extends StatelessWidget {
       alignment: Alignment.bottomCenter,
       child: SizedBox(
         width: MediaQuery.of(context).size.width,
-        height: 199,
+        height: 210,
         child: Stack(
           children: [
             Padding(
-              padding: const EdgeInsets.only(top: 16.0),
+              padding: const EdgeInsets.only(top: 10.0),
               child: Container(
                 width: double.infinity,
                 decoration: const BoxDecoration(
@@ -26,11 +27,29 @@ class Background extends StatelessWidget {
               ),
             ),
             Container(
-              width: double.infinity,
               decoration: const BoxDecoration(
                 image: DecorationImage(
                   fit: BoxFit.fill,
                   image: AssetImage('assets/images/Rectangle_1.png'),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 49),
+                child: MyElevatedButton(
+                  onPressed: () {},
+                  borderRadius: BorderRadius.circular(18),
+                  width: 125,
+                  child: const Center(
+                    child: Text('Sign up',
+                        style: TextStyle(
+                          color: Color.fromRGBO(71, 70, 70, 1.0),
+                          fontWeight: FontWeight.w900,
+                          fontSize: 16.5,
+                        )),
+                  ),
                 ),
               ),
             )
